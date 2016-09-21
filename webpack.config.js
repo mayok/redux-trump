9835fa7dca99f5e1ce6c51402902225e42fd6a60
Naohiro IWAMOTO <naodai24@gmail.com>
2016-09-21 15:50:04 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.jsx'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-2']
+        }
+      },
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader', 'eslint-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    hot: true,
+    contentBase: './dist',
+  },
+  eslint: {
+    configFile: './.eslintrc'
+  }
+};
